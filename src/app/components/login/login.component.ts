1362198b2809240c8a35c01f929aa1a0d386e8cd
Eusebio Joveth <yurijoveth@gmail.com>
2023-01-31 12:20:49 +0100

    Implementando a funcionalidade de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import {FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/Interfaces/login';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+import { UtilidadeService } from 'src/app/Reutilizavel/utilidade.service';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +13,47 @@ import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
+  ocultarPassword:boolean = true;
+  mostrarLoading:boolean = false;
+
+  constructor(
+    private _formBuilder: FormBuilder, private _usuarioService: UsuarioService,
+    private _utilidadeService: UtilidadeService, private router: Router
+
+  ) {
+    this.formularioLogin = this._formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  iniciarSessao(){
+    this.mostrarLoading = true;
+
+    const request: Login = {
+      email: this.formularioLogin.value.email,
+      password: this.formularioLogin.value.password
+    }
+
+    this._usuarioService.iniciarSesao(request).subscribe({
+      next: (data) =>{
+        if(data.status){
+          this._utilidadeService.guardarSessaoUsuario(data.valor);
+          this.router.navigate(["pages"])
+        }else
+        this._utilidadeService.mostrarAlerta("Email ou Senha Errado", "Opps!");
+      },
+      complete: () =>{
+        this.mostrarLoading = false;
+      },
+      error: () => {
+        this._utilidadeService.mostrarAlerta("Houve um Erro", "Opps!")
+      }
+    })
   }
 
 }
